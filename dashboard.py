0532eee2789e098520d89dbdc46a7c00fcdea1b9
Muhammad Hanif <63761728+hniff@users.noreply.github.com>
2024-06-24 18:57:36 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
 import zipfile
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 st.set_page_config(
     page_title="Bike Sharing Data Analysis Dashboard",
